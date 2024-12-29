7774cecea5ebb4ea189921c1969423e081e57d4f
Mohammad Anas Siddiqui <MoAnasSiddiqui@users.noreply.github.com>
2024-12-30 01:00:03 +0500

    added new feature to edit and add new notes

--- frontend/src/pages/Home/AddEditNotes.jsx
+++ frontend/src/pages/Home/AddEditNotes.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TagInput from '../../components/Input/TagInput'
+import { MdClose } from "react-icons/md"
 
-const AddEditNotes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const AddEditNotes = ({ noteData, type, onClose }) => {
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [tags, setTags] = useState([])
+    const [error, setError] = useState(null);
+
+    const addNewNote= async () => {};
+
+    const editNote = async () => {};
+
+    const handleAddNote = () => {
+        if (!title) {
+            setError("Please enter the title")
+            return;
+        }
+        if (!content) {
+            setError("Please enter the content")
+            return;
+        }
+        setError("");
+
+        if (type === 'edit'){
+            editNote()
+        } else {
+            addNewNote()
+        }
+
+        return
+    }
+
+    return (
+        <div className='relative'>
+            <button className='w-7 h-7 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-red-500' onClick={onClose}>
+                <MdClose className="text-xl" />
+            </button>
+
+            <div className='flex flex-col gap-2'>
+                <label className='input-label'>Title</label>
+                <input
+                    type='text'
+                    className='text-2xl text-slate-950 outline-none'
+                    placeholder='Go to gym at 5:00'
+                    value={title}
+                    onChange={({ target }) => setTitle(target.value)}
+                ></input>
+            </div>
+
+            <div className='flex flex-col gap-2 mt-4'>
+                <label className='input-label'>Content</label>
+                <textarea
+                    type='text'
+                    className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+                    placeholder='Content'
+                    rows={10}
+                    value={content}
+                    onChange={({ target }) => setContent(target.value)}
+                />
+            </div>
+            <div className='mt-3'>
+                <label className='input-label'>Tags</label>
+                <TagInput tags={tags} setTags={setTags} />
+            </div>
+
+            {error && <p className='text-red-500 text-xs pt'>{error}</p>}
+
+            <button className='btn-primary font-medium mt-5 p-3'
+                onClick={handleAddNote}>
+                Add
+            </button>
+        </div>
+    )
 }
 
 export default AddEditNotes
