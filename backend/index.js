a433a4305a6ab8e709829ca65eb68f8c5786d66b
Mohammad Anas Siddiqui <MoAnasSiddiqui@users.noreply.github.com>
2024-12-30 13:36:06 +0500

    created login and create-account API's

--- backend/index.js
+++ backend/index.js
@@ -58,23 +58,70 @@ app.post("/create-account", async (req, res) => {
     }
 
     const user = new User({
-        fullName, 
+        fullName,
         email,
         password
     })
 
     await user.save();
 
-    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, {
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "36000m",
     })
 
-    return res.json ({
+    return res.json({
         error: false,
         user,
         accessToken,
         message: "Registration Successful",
     })
+});
+
+app.post("/login", async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Password is required" });
+    }
+
+    const userInfo = await User.findOne({ email: email })
+
+    if (!userInfo) {
+        return res
+            .status(400)
+            .json({
+                error: true,
+                message: "User does not exist"
+            })
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo };
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m",
+        })
+
+        return res.json({
+            error: false,
+            message: "Login Successful",
+            email,
+            accessToken
+        })
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: "Credentials are invalid"
+        })
+    }
 });
 
 app.listen(8000);
