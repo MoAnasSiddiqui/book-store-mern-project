0231cbd82961db17fcd13ef4b8e29981f471cad5
Mohammad Anas Siddiqui <MoAnasSiddiqui@users.noreply.github.com>
2024-12-30 12:51:54 +0500

    created create-account API

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString)
 
+const User = require("./models/user.model")
+
 const express = require("express");
 const cors = require("cors")
 const app = express();
@@ -26,6 +28,53 @@ app.get("/", (req, res) => {
 
 app.post("/create-account", async (req, res) => {
 
+    const { fullName, email, password } = req.body;
+
+    if (!fullName) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Full Name is required" });
+    }
+
+    if (!email) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res
+            .status(400)
+            .json({ error: true, message: "Password is required" });
+    }
+
+    const isUser = await User.findOne({ email: email })
+
+    if (isUser) {
+        return res.json({
+            error: true,
+            message: "User already exists"
+        })
+    }
+
+    const user = new User({
+        fullName, 
+        email,
+        password
+    })
+
+    await user.save();
+
+    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m",
+    })
+
+    return res.json ({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Successful",
+    })
 });
 
 app.listen(8000);
